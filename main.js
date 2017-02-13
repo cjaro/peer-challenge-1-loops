944bbf8af914b61ae457a90a5b4ca0ec54bd3e8a
Catherine J <cjarocki@gmail.com>
2017-02-13 16:42:31 -0600

    more peer challenge work

--- main.js
+++ main.js
@@ -4,36 +4,73 @@
 
 $(document).ready(function(){
 
-var employeeArray = [{name: 'Bob', yearsOfExperience: 4}, {name: 'Carla', yearsOfExperience: 5}, {name: 'John', yearsOfExperience: 10}];
-var totalExperience = 0;
+  var employeeArray = [{name: 'Bob', yearsOfExperience: 4}, {name: 'Carla', yearsOfExperience: 5}, {name: 'John', yearsOfExperience: 10}];
+  var totalExperience = 0;
 
   employeeArray.forEach(function(employee, i){
-    var yearsOfExperience = employee.yearsOfExperience;
-    totalExperience += employee.yearsOfExperience;
+    var yearsOfExperience = parseInt(employee.yearsOfExperience);
+    totalExperience += parseInt(employee.yearsOfExperience);
 
-  $('#employeeExperience').append(
-    '<tr>' +
-    '<td>' + employee.name + '</td>' +
-    '<td>' + employee.yearsOfExperience + '</td>' +
-    '</tr>');
+    $('#employeeExperience').append(
+      '<tr>' +
+      '<td>' + employee.name + '</td>' +
+      '<td>' + employee.yearsOfExperience + '</td>' +
+      '</tr>');
 
-  console.log(totalExperience);
+      $('#totalYears').text(totalExperience);
 
-  return totalExperience;
-});
-});
+      console.log(totalExperience);
+    }); //ending employee array function
+
+    $('#submitNew').on('click', 'button', function(){
+      var name = $('#name').val();
+      var yearsOfExperience = $('#yearsOfExperience').val();
+
+      function Employee(name, yearsOfExperience) {
+        this.name = name;
+        this.yearsOfExperience = yearsOfExperience;
+      }
+      var newEmployee = new Employee(name, yearsOfExperience);
+      employeeArray.push(newEmployee);
+
+      $('#employeeExperience').empty();
+      $('#totalYears').empty();
+
+      employeeArray.forEach(function(employee, i){
+        var yearsOfExperience = parseInt(employee.yearsOfExperience);
+        totalExperience += parseInt(employee.yearsOfExperience);
+
+      $('#employeeExperience').append(
+        '<tr>' +
+        '<td>' + employee.name + '</td>' +
+        '<td>' + employee.yearsOfExperience + '</td>' +
+        '</tr>');
+
+        $('#totalYears').text(totalExperience);
+
+        console.log(totalExperience);
+
+
+      });
+    });
+  });
 
 
 
-// var totalExperience = 0;
-//
-//   for (var i = 0; i < employeeArray.length; i++) {
-//     var people = employeeArray[i];
-//     var yearsOfExperience = people.yearsOfExperience;
-//     totalExperience += people.yearsOfExperience;
-//   };
+// need to not repeat previously entered data yearsOfExperience
 
 
 
 
-//  we need to pick out yearsOfExperience from the array and add them up
+    // var totalExperience = 0;
+    //
+    //   for (var i = 0; i < employeeArray.length; i++) {
+    //     var people = employeeArray[i];
+    //     var yearsOfExperience = people.yearsOfExperience;
+    //     totalExperience += people.yearsOfExperience;
+    //   };
+
+
+
+
+    //  we need to pick out yearsOfExperience from the array and add them up
